4af5edbea869ff7bd5523871d63d226d0b6427d8
WayneMusungu <waynemuyera17@gmail.com>
2022-06-11 21:33:17 +0300

    Create a Profile model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile (models.Model):
+    bio = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='profile_images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    
+    def __str__(self):
+        return self.bio
+    
+    def save_projects(self):
+        self.save()
+        
+    def delete_projects(self):
+        self.delete()
