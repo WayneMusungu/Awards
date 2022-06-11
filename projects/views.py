b8fdc6780d80810a7097388c5d0530d84d459dec
WayneMusungu <waynemuyera17@gmail.com>
2022-06-11 21:30:04 +0300

    Create a base html and extend it to index.html

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+import datetime as dt
 # Create your views here.
+def welcome(request):
+    date = dt.date.today()
+    return render (request, 'index.html',{"date": date})
