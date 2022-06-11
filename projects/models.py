74aa37dceb1e5cfc6b8faf4a0a591e2e0d06ad3b
WayneMusungu <waynemuyera17@gmail.com>
2022-06-11 21:47:22 +0300

    Create a project model class

--- projects/models.py
+++ projects/models.py
@@ -16,3 +16,39 @@ class Profile (models.Model):
         
     def delete_projects(self):
         self.delete()
+        
+        
+class Projects (models.Model):
+    title = models.CharField(max_length=60)
+    image = models.ImageField(upload_to='profile_images')
+    description = models.TextField()
+    location = models.CharField(max_length=50, blank=True)
+    technologies_used =models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    link = models.CharField(max_length=60)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    @classmethod
+    def display_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def search_by_title(cls, title):
+        image= cls.objects.filter(title__title__icontains=title)
+        return image
+
+    @classmethod
+    def get_projects(cls):
+        return cls.objects.all()
+    @classmethod
+    def update_project(cls,pk):
+        return cls.objects.get(id=pk)
+
+    def __str__(self):
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404, JsonResponse
 import datetime as dt
+from rest_framework import response
 # Create your views here.
 def welcome(request):
     date = dt.date.today()
     return render (request, 'index.html',{"date": date})
+
