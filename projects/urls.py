60305f8a36c64f77b095524dfa3a487aa436feed
WayneMusungu <waynemuyera17@gmail.com>
2022-06-11 22:26:23 +0300

    Create views for functions

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
    path('', views.welcome, name='welcome'),
+   path('projects/', views.projects, name='projects'),
+   path('otima/',views.profileList.as_view() , name='otima'),
+   path('projo/',views.projectsList.as_view() , name='projo'),
+   path('create/',views.projectsCreate.as_view() , name='create'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . serializer import ProfileSerializer, ProjectsSerializer
 from . models import Profile, Projects
-from projects import serializer
+
 from rest_framework import status
 
 # Create your views here.
