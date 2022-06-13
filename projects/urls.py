ffdd307f477e0c26720b1145e0abce3eceb7599b
WayneMusungu <waynemuyera17@gmail.com>
2022-06-13 12:01:21 +0300

    Adjust the login and registration templates

--- projects/urls.py
+++ projects/urls.py
@@ -5,8 +5,8 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
    path('', views.index, name='index'),
    path('projects/', views.projects, name='projects'),
-   path('api/profile',views.profileList.as_view() , name='otima'),
-   path('api/projects',views.projectsList.as_view() , name='projo'),
+   path('api/profile/',views.profileList.as_view() , name='otima'),
+   path('api/projects/',views.projectsList.as_view() , name='projo'),
    path('api/create/',views.projectsCreate.as_view() , name='create'),
    path('details/<int:project_id>', views.project_details, name='image'),
    path('delete_post/<str:pk>/', views.delete_post, name='delete-post'),
